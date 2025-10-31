5145feb398f1ab7f00d83d63a555e2fbef4bb4bf
Zhao0100 <130882285+Zhao0100@users.noreply.github.com>
2025-10-31 21:19:14 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-LOG_DIR="/home/jovyan/boomcheng-work-shcdt/zhaoshihao/code/UHD2/logs"
+LOG_DIR="~"
 mkdir -p $LOG_DIR
-CUDA_VISIBLE_DEVICES=0 python inference.py > $LOG_DIR/uhdres_UHD_LL_1031.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python inference.py > $LOG_DIR/output.log 2>&1 &
