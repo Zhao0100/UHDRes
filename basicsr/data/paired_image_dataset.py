15b02c25269a15f8163fc1ee6b3ed0fab67e0f66
Zhao0100 <130882285+Zhao0100@users.noreply.github.com>
2025-11-10 17:34:18 +0800

    Update paired_image_dataset.py

--- basicsr/data/paired_image_dataset.py
+++ basicsr/data/paired_image_dataset.py
@@ -127,7 +127,7 @@ class PairedImageDataset(data.Dataset):
             # random crop
             img_gt, img_lq = paired_random_crop(img_gt, img_lq, gt_size, scale, gt_path)
             # flip, rotation
-            img_gt, img_lq = augment([img_gt, img_lq], self.opt['use_flip'], self.opt['use_rot'])
+            img_gt, img_lq = augment([img_gt, img_lq], self.opt['use_hflip'], self.opt['use_rot'])
 
         # color space transform
         if 'color' in self.opt and self.opt['color'] == 'y':
